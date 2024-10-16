0c32eebda66b03591e84bcd171c46583f7701386
shelavale3795 <pallavishelavale3795@gmail.com>
2024-10-17 00:12:08 +0530

    change in server file

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import orderRouter from "./routes/orderRoute.js"
 
 //app config
 const app = express()
-const port = 4000
+const port = process.env.PORT || 4000
 
 //middleware
 app.use(express.json()) //whenever we will get request from the frontend to backend that will be parsed using these jsx
