308b32526b3b8790d8d71a2dfcdf06bc83f9de8c
shelavale3795 <pallavishelavale3795@gmail.com>
2024-11-11 00:25:14 +0530

    url

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -96,7 +96,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  // const url = "http://localhost:4000";
+  const url = "https://tomato-ivdz.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFood_list] = useState([]);
 
